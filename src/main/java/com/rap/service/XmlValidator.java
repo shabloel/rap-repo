f823b30c64c901764a38d38ef2ea82b5006f13ba
chris <cgirecruitment19@gmail.com>
2020-12-28 11:55:51 +0100

    validation complete

--- src/main/java/com/rap/service/XmlValidator.java
+++ src/main/java/com/rap/service/XmlValidator.java
@@ -1,23 +1,24 @@
 package com.rap.service;
 
 import org.springframework.stereotype.Service;
-
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.stax.StAXSource;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.StringReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 
 @Service
-public class XmlValidatorService {
+public class XmlValidator {
 
     /** The validator. */
     private static Validator validator;
@@ -30,8 +31,8 @@ public class XmlValidatorService {
      */
     public static void init() throws SAXException {
 
-        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI);
-        Schema schema = schemaFactory.newSchema(new File("xsd/upa_2021_request.xsd"));
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(new StreamSource(XmlValidator.class.getClassLoader().getResource("xsd/upa_2021_request.xsd").toExternalForm()));
         validator = schema.newValidator();
     }
 
@@ -39,7 +40,7 @@ public class XmlValidatorService {
     /**
      * Validate.
      *
-     * @param xml
+     * @param input
      *            the xml
      * @return true, if successful
      * @throws SAXException
@@ -47,8 +48,9 @@ public class XmlValidatorService {
      * @throws IOException
      *             Signals that an I/O exception has occurred.
      */
-    public static boolean validate(String xml) throws SAXException, IOException {
-
+    public static boolean validate(File input) throws SAXException, IOException, XMLStreamException {
+        StAXSource source = null;
+        FileInputStream inputStream = null;
         boolean returnValue = false;
 
         if (validator == null) {
@@ -56,32 +58,21 @@ public class XmlValidatorService {
         }
 
         try {
-            validator.validate(new StreamSource(new StringReader(xml)));
+            XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+            inputStream = new FileInputStream(input);
+            XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(inputStream);
+            source = new StAXSource(xmlStreamReader);
+
+            validator.validate(source);
             returnValue = true;
-        } catch (SAXException | IOException exception) {
+        } catch (SAXException | IOException | XMLStreamException exception) {
             throw exception;
         }
 
         return returnValue;
     }
 
-    /**
-     * Validate.
-     *
-     * @param path
-     *            the path
-     * @return true, if successful
-     * @throws SAXException
-     *             the SAX exception
-     * @throws IOException
-     *             Signals that an I/O exception has occurred.
-     */
-    public static boolean validate(Path path) throws SAXException, IOException {
 
-        String xml = new String(Files.readAllBytes(path));
-
-        return validate(xml);
-    }
 
     /**
      * Gets the validator.
--- src/test/java/com/rap/restservicevalidator/RestServiceValidatorApplicationTests.java
+++ src/test/java/com/rap/restservicevalidator/RestServiceValidatorApplicationTests.java
@@ -1,13 +1,36 @@
 package com.rap.restservicevalidator;
 
+import com.rap.service.XmlValidator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.xml.sax.SAXException;
+
+import javax.xml.stream.XMLStreamException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 @SpringBootTest
 class RestServiceValidatorApplicationTests {
 
+	private static final String PATH = "src/test/resources/berichten";
+
+    /**
+     * Valideer incident-melding bericht.
+     *
+     * @throws SAXException the sax exception
+     * @throws IOException  the io exception
+     */
+
 	@Test
-	void contextLoads() {
+	void validateUPA_Regulier() throws SAXException, IOException, XMLStreamException {
+	    Path path = FileSystems.getDefault().getPath(PATH, "UPA_Regulier_2021_upa.xml");
+        assertEquals(true, XmlValidator.validate(new File("src/test/resources/berichten/UPA_Regulier_2021_upa.xml")));
 	}
 
 }
